29a6852c1171bcf38b59482bcbc23ffcbaed5d7e
jakedwng <jakedwng@gmail.com>
2023-01-09 20:43:09 -0500

    Add HtmlWebpackPlugin to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './client/dist'),
+  },
+	plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Test',
+    }),
+  ],
+	module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
